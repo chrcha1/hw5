78b23f560a15f8f2469034f7d568f0869d22a280
Christelle Chatelain <cchatela@usc.edu>
2023-04-06 21:51:18 +0000

    passed tests

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleHelper(const AvailabilityMatrix& avail, int m, int d, int k,int n, int i, int j, DailySchedule& sched);
+bool isValid(const AvailabilityMatrix& avail, unsigned int worker, int m, int n, int d, DailySchedule& sched);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -38,8 +40,61 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // initializing  sched matrix w INVALID_ID vals
+    for(size_t i = 0; i < avail.size(); i++)
+    {
+        vector<Worker_T> row(dailyNeed, INVALID_ID);
+        sched.push_back(row);
+    }
 
+    int n = avail.size();
+    int k = avail[0].size();
 
-
+    return scheduleHelper(avail, maxShifts, dailyNeed, k, n, 0, 0, sched);
 }
 
+// recursively schedules workers
+bool scheduleHelper(const AvailabilityMatrix& avail, int m, int d, int k,int n, int i, int j, DailySchedule& sched)
+{
+    if (i == n) return true; // all days scheduled
+    if (j == d) return scheduleHelper(avail, m, d, k, n, i + 1, 0, sched); // move to next day
+
+    for (int currWorker = 0; currWorker < k; currWorker++)
+    {
+        sched[i][j] = (Worker_T) currWorker;
+        if (isValid(avail, currWorker, m, n, d, sched))
+        {
+            if (scheduleHelper(avail, m, d, k, n, i, j + 1, sched)) return true;
+        }
+    }
+
+    // if no valid worker found---backtrack
+    sched[i][j] = INVALID_ID;
+    return false;
+}
+
+// check if curr sched valid
+bool isValid(const AvailabilityMatrix& avail, unsigned int worker, int m, int n, int d, DailySchedule& sched)
+{
+    int numShifts = 0;
+    int numShiftsPerDay = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < d; j++)
+        {
+            if (sched[i][j] == worker)
+            {
+                if (!avail[i][worker]) return false; // worker not available
+                numShifts++;
+                numShiftsPerDay++;
+            }
+        }
+
+        if (numShiftsPerDay > 1) return false; // >1 shift per day
+        numShiftsPerDay = 0;
+    }
+
+    return numShifts <= m; // are worker's shifts within limit
+		//yeeee
+}
--- wordle.cpp
+++ wordle.cpp
@@ -1,19 +1,16 @@
-#ifndef RECCHECK
 // For debugging
 #include <iostream>
 // For std::remove
 #include <algorithm> 
 #include <map>
 #include <set>
-#endif
-
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
 
 
-// Add prototypes of helper functions here
-
+// Add prototypes of helper functions her
+void helper(int N, int idx, const string& in, const string& floating, const set<string>& dict, set<string>& res);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +19,78 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+		int N = in.length();
+    int idx = 0;
+    set<string> res;
+    helper(N, idx, in, floating, dict, res);
 
+    return res;
 }
 
 // Define any helper functions here
+void helper(int N, int idx, const string& in, const string& floating, const set<string>& dict, set<string>& res)
+{
+    int dashCnt = count(in.begin(), in.end(), '-');
+    string curr = in;
+    string yellows = floating;
+
+    // declaring base cases
+    if (idx == N && floating.empty() && dict.find(in) != dict.end())
+    {
+        res.insert(curr);
+        return;
+    }
+    else if (idx == N && floating.empty())
+    {
+        return;
+    }
+
+    // if dash
+    if (curr[idx] == '-')
+    {
+        int numAlpha = dashCnt - yellows.size();
+
+        // if numdashes > yellows.size()
+        if (numAlpha > 0)
+        {
+            // try fill w whole alphabet
+            for (char j = 'a'; j <= 'z'; j++)
+            {
+                curr[idx] = j;
+
+                // if not floating char
+                if (yellows.find(j) == string::npos)
+                {
+                    helper(N, idx + 1, curr, yellows, dict, res);
+                }
+                else
+                {
+                    // removing the floating char & recurse
+                    yellows.erase(yellows.find(j), 1);
+                    helper(N, idx + 1, curr, yellows, dict, res);
+
+                    // resetting yellows
+                    yellows = floating;
+                }
+            }
+        }
+        else if (numAlpha == 0)
+        {
+            // if no alpha char left, try floating
+            for (size_t k = 0; k < yellows.size(); k++)
+            {
+                curr[idx] = yellows[k];
+                yellows.erase(k, 1);
+                helper(N, idx + 1, curr, yellows, dict, res);
+
+                // resetting yellows
+                yellows = floating;
+            }
+        }
+    }
+    else
+    {
+        // move to next char
+        helper(N, idx + 1, curr, yellows, dict, res);
+    }
+}
